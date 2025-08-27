c9898d222d2c0b73ff7888396995c804d60da3c8
KatFoxx <f.lentge@pm.me>
2025-08-27 10:31:11 +0200

    Add expireTime configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   compiler: { styledComponents: true },
   i18n: nextI18NextConfig.i18n,
+  expireTime: 3600,
 };
 
 export default nextConfig;
