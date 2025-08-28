8239047bee384c9441d9e25ff007571823e52564
KatFoxx <f.lentge@pm.me>
2025-08-28 09:43:00 +0200

    Fix Privacy Policy link case sensitivity and restore PrivacyPolicy page component

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     const { t } = useTranslation('translation');
     return (<footer className="bg-black text-ivory flex justify-center gap-4">
         <Link href={"/impressum"} className="text-white underline hover:text-red-500">Impressum</Link>
-        <Link href={"/privacyPolicy"} className="text-white underline hover:text-red-500">{t('Privacy Policy')}</Link>
+        <Link href={"/PrivacyPolicy"} className="text-white underline hover:text-red-500">{t('Privacy Policy')}</Link>
     </footer>
     )
 }
